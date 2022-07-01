58dbd3ad2fe8686893bee9eba433391511fa7d4a
BBongKim <72081383+BBongKim@users.noreply.github.com>
2022-07-01 16:15:34 +0900

    7월 1일

---
+++ Programmers/Level 4/징검다리/solution.kt
@@ -0,0 +1,36 @@
+// 이분탐색 문제
+// 어떤 값을 이분 탐색할지, 어떤 값을 기준 left, right를 옮길지 정하기 어려웠다.
+// 다른 분 풀이를 보니, 바위 사이의 최솟값을 탐색으로 찾고, 이 최솟값을 가지기 위해 지운 바위의 개수를 가지고 left, right를 옮겼다.
+// 만약 해당 최솟값을 가지기 위해 지운 바위의 수가 n보다 크다면 최솟값 기준을 줄여야 한다. 따라서 right를 (mid - 1)로 옮긴다.
+// 만약 n보다 같거나 작다면, 우선 답의 후보로 지정해두고(해당 문제는 답이 될 수 있는 가장 큰 n을 찾는 것이기 때문에 max 연산을 한다.), left를 (mid + 1)로 옮긴다.
+
+class Solution {
+    fun solution(distance: Int, rocks: IntArray, n: Int): Int {
+        var answer = -1
+        val rockList = rocks.toList().sorted()
+        
+        var left = 1
+        var right = distance
+       
+        while (left <= right) {
+             val mid = (left + right) / 2
+                
+             var prev = 0
+             var remove = 0
+             for (rock in rockList) {
+                 if (rock - prev >= mid) prev = rock
+                 else remove++
+             }
+             
+             if (distance - prev < mid) remove++
+             
+             if (remove > n) right = mid - 1
+             else {
+                 answer = maxOf(answer, mid)
+                 left = mid + 1
+             }
+        }
+        
+        return answer
+    }
+}
