a626b9815b3565ea7e8a6acf3ac0e63bfcaf622c
BBongKim <72081383+BBongKim@users.noreply.github.com>
2023-02-01 17:20:54 +0900

    2023년 2월 1일

---
+++ Baekjoon/그리디/2258 정육점/Main.kt
@@ -0,0 +1,53 @@
+// 정육점
+// https://www.acmicpc.net/problem/2258
+
+// 그리디, 정렬
+
+// 1. 가격 내림차순 -> 무게 오름차순으로 정렬한다.
+// 2. 이전과 같은 가격이면 가격을 더해주고, 더 비싸다면 현재 고기 가격으로 설정한다.
+// 3. 무게 조건을 만족할 때마다, 최솟값을 갱신해주어야한다.
+
+// 풀이는 알았는데, 자꾸 오답이 나와서, 여러 풀이를 참고해서, flag 값을 넣어줬는데
+// 정답처리됨... answer가 MAX_VALUE가 아닐때가 있는건가...?
+
+package Study05
+
+data class Meat(val weight: Int, val cost: Int)
+
+object Main3 {
+    fun solution(N: Int, M: Int, meats: List<Meat>): Int {
+        var answer = Int.MAX_VALUE
+        var totalCost = 0
+        var totalWeight = 0
+        var hasAnswer = false
+
+        for (i in meats.indices) {
+            totalWeight += meats[i].weight
+
+            if (i > 0 && meats[i - 1].cost == meats[i].cost) {
+                totalCost += meats[i].cost
+            } else {
+                totalCost = meats[i].cost
+            }
+
+            if (totalWeight >= M) {
+                hasAnswer = true        // 왜 플래그 넣으니까 정답이지...?
+                answer = minOf(answer, totalCost)
+            }
+        }
+
+        return if (hasAnswer) answer else -1
+    }
+}
+
+fun main() {
+    val (N, M) = readln().split(" ").map { it.toInt() }
+    val meats = mutableListOf<Meat>()
+
+    repeat(N) {
+        val (w, c) = readln().split(" ").map { it.toInt() }
+        meats.add(Meat(w, c))
+    }
+
+    println(Main3.solution(N, M, meats.sortedWith(compareBy<Meat> { it.cost }.thenByDescending { it.weight })))
+}
